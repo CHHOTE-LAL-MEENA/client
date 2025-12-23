364b7aeca8ecfbee1f91c94a4a8881e258ab6f72
CHHOTE LAL MEENA <meenachhotelal027@gmail.com>
2025-12-23 09:20:17 +0530

    Update fetchWithAuth.js

--- src/utils/api/fetchWithAuth.js
+++ src/utils/api/fetchWithAuth.js
@@ -2,7 +2,7 @@ import { store } from "../../redux/store";
 export async function fetchWithAuth(url, options = {}) {
   const token = store.getState().user?.token;
 
-  const res = await fetch(`https://server-red-sigma-78.vercel.app/api${url}`, {
+  const res = await fetch(`https://server-mu-one-98.vercel.app/api${url}`, {
     ...options,
     headers: {
       "Content-Type": "application/json",
